caca9fcaf35525eac1698b64b87faf09e9b69669
Edwin-A.J <dezwhy97@gmail.com>
2026-06-16 01:05:50 +0100

    fix: removed dangling dependency

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 INSTALLED_APPS = [
-    'daphne',
+    # 'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
